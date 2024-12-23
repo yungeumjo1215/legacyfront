57fb67a36fef0ea0ba09ef852fbe0169f1781a4b
yungeumjo1215 <elec0807@gmail.com>
2024-12-23 13:15:00 +0900

    a

--- src/components/chatbot/ActionProvider.js
+++ src/components/chatbot/ActionProvider.js
@@ -22,7 +22,7 @@ class ActionProvider {
         messages: [...prevState.messages, loadingMessage],
       }));
 
-      const response = await fetch("http://localhost:8000/chat", {
+      const response = await fetch("https://back.a.ringgo.site/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
