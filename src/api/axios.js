5f65befe65e8a0b224da2cab772cd156fffcf50d
yungeumjo1215 <elec0807@gmail.com>
2024-12-23 12:02:18 +0900

    ax

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://back.a.ringgo.site/",
   headers: {
     "Content-Type": "application/json",
   },
